1eac5280901989a5f3261c51cd0601d27a2b0949
Dan Paddock <danpaddk@udel.edu>
2017-02-09 13:23:09 -0500

    Implemented Rectangle.Area(), added Github name

--- src/pkgMain/cMain.java
+++ src/pkgMain/cMain.java
@@ -4,6 +4,10 @@ public class cMain {
 
 	public static void main(String[] args) {
 
+		/*
+		 *  My Github Account Name: DanPaddock1
+		 *  
+		 */
 		Rectangle rec = new Rectangle(1.5, 2.5);		
 		System.out.println("Rectangle Area: " + rec.Area());
 	}
